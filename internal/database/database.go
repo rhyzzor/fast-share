8b8ba292e6a56cc8d7be039edd7bcd17b8eed268
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-02-03 14:34:44 -0300

    feat: add connect database

---
+++ internal/database/database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connect(uri string) (*mongo.Database, error) {
+	clientOptions := options.Client().ApplyURI(uri)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client.Database("fast-share"), nil
+}
